be0e39e6fbf374742ff31e9f6d56594d633681d2
Sascha Ludwig <sascha.ludwig@bs-elmshorn.de>
2021-10-28 14:31:21 +0200

    status

--- components/post_schueler_id_bearbeiten.rb
+++ components/post_schueler_id_bearbeiten.rb
@@ -1,2 +1,33 @@
-#=begin Formular mit vorausgefüllten Feldern für Name und Klasse. Passwort-Feld leer.
-#Speichern-Knopf POST /schueler/:id/bearbeiten =end
+#Konsistenzprüfung:
+#Name nicht der leere String und nicht mit anderer id verwendet
+#Klasse nicht der leere String
+#Falls inkonsistent: Weiterleitung GET /schueler/:id/bearbeiten
+
+#Sonst:
+
+#Datensatz zu id aus Datenbank abfragen
+#Name mit Eingabe überschreiben
+#Wenn Passwort nicht leer, Hash berechnen und im Datensatz eintragen
+#Id zu eingegebener Klasse bestimmen (ggf. neu eintragen)
+#Id der Klasse im Datensatz eintragen
+#Schueler-Datensatz in der Datenbank aktualisieren
+#Weiterleitung GET /schueler
+
+post "/schueler/:id/bearbeiten" do  |sid|
+    user = current_user 
+    schueler = {
+        'Name' => params['Name'],
+        'Klasse' => params['Klasse'],
+        'Passwort' => params['Passwort']
+      }
+    #if (schueler["Klasse"]==nil || schueler["Name"]!=user["Name"] || schueler["Name"].size<0 ) # Konsistenzprüfung
+    #    redirect to "/schueler/:"+sid+"/bearbeiten"
+    #end
+
+    #if (get_status(current_user)=="Admin") # wenn Admin arbeiten darf
+    #    
+    #end
+    
+    redirect to "/schueler"
+    
+end
